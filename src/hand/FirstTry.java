484f8f11bfdf4b6e7983fe79c4bbdede8772bdf1
Martin <mr.mmaurer@gmail.com>
2012-10-12 00:27:34 +0200

    Initial Version

---
+++ src/hand/FirstTry.java
@@ -0,0 +1,57 @@
+package hand;
+
+import processing.core.PApplet;
+import SimpleOpenNI.SimpleOpenNI;
+
+public class FirstTry extends PApplet {
+	SimpleOpenNI context;
+		
+	public void setup(){
+	  context = new SimpleOpenNI(this,SimpleOpenNI.RUN_MODE_MULTI_THREADED);   
+	  
+	  // Quellen aktivieren
+	  context.enableDepth();  
+	  //context.enableIR();
+	  context.enableRGB();
+	  //context.enableHands();
+	  //context.enableGesture();
+	  
+	  // Alle Bones
+	  context.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL);
+	  
+	  // Spiegeln
+	  context.setMirror(true);
+	  
+	  // add focus gestures 
+	  // context.addGesture("Wave");
+	  // context.addGesture("Click");
+	  // context.addGesture("RaiseHand");  
+	  
+	  //Enable Scene
+	  context.enableScene(640,480,60);  
+	  //context.enableScene(700,720,60);
+	  
+	  stroke(255);
+	  strokeWeight(12);
+	  noFill();  
+	  smooth();  
+	  //size(context.depthWidth(), context.depthHeight());
+	  frameRate(60);
+	  size(1280, 480);
+	}
+	
+	public void draw(){
+		// Kinect updaten --> WICHTIG
+		context.update();
+		//Depth Image
+		image(context.depthImage(),0,0);
+		//RGB Image
+		image(context.rgbImage(),640,0);
+	}
+	
+	// Calback Neuer User
+	public void onNewUser(int userId){
+		println("Neuer User");		  
+	}
+
+}
---
+++ src/test/ProcessingTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import processing.core.*;
+
+public class ProcessingTest extends PApplet {
+	int rad = 60;        // Width of the shape
+	float xpos, ypos;    // Starting position of shape    
+
+	float  xspeed = 2.8f;  // Speed of the shape
+	float yspeed = 2.2f;  // Speed of the shape
+
+	int xdirection = 1;  // Left or Right
+	int ydirection = 1;  // Top to Bottom
+
+	public void setup() 
+	{
+	  size(640, 360);
+	  noStroke();
+	  frameRate(60);
+	  ellipseMode(RADIUS);
+	  // Set the starting position of the shape
+	  xpos = width/2;
+	  ypos = height/2;
+	}
+
+	public void draw() 
+	{
+	  background(102);
+	  
+	  // Update the position of the shape
+	  xpos = xpos + ( xspeed * xdirection );
+	  ypos = ypos + ( yspeed * ydirection );
+	  
+	  // Test to see if the shape exceeds the boundaries of the screen
+	  // If it does, reverse its direction by multiplying by -1
+	  if (xpos > width-rad || xpos < rad) {
+	    xdirection *= -1;
+	  }
+	  if (ypos > height-rad || ypos < rad) {
+	    ydirection *= -1;
+	  }
+
+	  // Draw the shape
+	  ellipse(xpos, ypos, rad, rad);
+	}
+
+}
